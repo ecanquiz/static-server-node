f0eef8e57f647f63e0d3808403acfde8802c1b2b
Ernesto Canquiz <caribestic123@gmail.com>
2025-08-03 16:07:26 -0400

    refactor: Clean code of getMimeType function

--- src/utils/getMimeType.ts
+++ src/utils/getMimeType.ts
@@ -3,22 +3,7 @@ import path from 'path';
 type ValidExtensions = '.jpg' | '.jpeg' | '.png' | '.gif' | '.pdf' | '.webp' | '.svg' | '.mp4' | '.mp3';
 type MimeTypeMap = Record<ValidExtensions, string>;
 
-// Function to determine the MIME type (optional)
-/*export default function getMimeType(filePath: string): string {
-  type MimeTypes =  '.jpg' | '.jpeg' | '.png' |  '.gif' | '.pdf';
-  const extname: MimeTypes  = path.extname(filePath).toLowerCase() as MimeTypes;
-  const mimeTypes = {
-    '.jpg': 'image/jpeg',
-    '.jpeg': 'image/jpeg',
-    '.png': 'image/png',
-    '.gif': 'image/gif',
-    '.pdf': 'application/pdf'
-  };
-  return mimeTypes[extname] || 'application/octet-stream';
-}*/
-
-
-
+// Function to determine the MIME type
 export default function getMimeType(filePath: string): string {
   if (typeof filePath !== 'string') return 'application/octet-stream';
   
@@ -35,5 +20,6 @@ export default function getMimeType(filePath: string): string {
   };
 
   const ext = path.extname(filePath).toLowerCase() as ValidExtensions;
+
   return mimeTypes[ext] || 'application/octet-stream';
 }
